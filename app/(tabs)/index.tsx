9d4d03f719db358968e27fb51362bdb1e5e1aafa
shaneak03 <shaneak03@gmail.com>
2025-05-19 10:17:47 +0800

    Set up supabase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,34 @@
+import { supabase } from "@/utils/supabase";
 import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
 
 export default function Index() {
   const textColor = useColorScheme() === "dark" ? "white" : "black";
   const router = useRouter();
+
+  const [datas, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data: data, error } = await supabase.from('data').select();
+
+        if (error) {
+          console.error('Error fetching data:', error.message);
+          return;
+        }
+
+        if (data && data.length > 0) {
+          setData(datas);
+        }
+      } catch (error : any) {
+        console.error('Error fetching todos:', error.message);
+      }
+    };
+
+    getData();
+  }, []);
 
   return (
     <View
