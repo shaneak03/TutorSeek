244ca7cbc7b09d8fc660291a56a9511a59ae0bff
shaneak03 <shaneak03@gmail.com>
2025-05-21 11:39:23 +0800

    Add pages, Fixes #8

---
+++ app/chatpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function chatpage() {
+  return (
+    <View>
+      <Text>chatpage</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
