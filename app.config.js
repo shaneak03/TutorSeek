042611cc01e3603f3de12b93444aa23668ffbc81
shaneak03 <shaneak03@gmail.com>
2025-05-27 10:47:31 +0800

    Update to node packages and environment variables

---
+++ app.config.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "TutorSeek",
+    slug: "TutorSeek",
+    version: "1.0.0",
+    userInterfaceStyle: "automatic",
+    android: {
+      package: "com.shaneak03.TutorSeek",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      versionCode: 1
+    },
+    extra: {
+      // These values come from your .env file or from environment variables set in Expo dashboard or eas.json
+      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+    }
+  }
+};
