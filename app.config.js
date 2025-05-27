489e290d5596f19daef2d6cc8e963045b9451bbe
shaneak03 <shaneak03@gmail.com>
2025-05-27 11:48:48 +0800

    APK works, fixes #18

--- app.config.js
+++ app.config.js
@@ -6,6 +6,8 @@ export default {
     slug: "TutorSeek",
     version: "1.0.0",
     userInterfaceStyle: "automatic",
+    scheme: "tutorseek",
+    owner: "shaneak03",
     android: {
       package: "com.shaneak03.TutorSeek",
       adaptiveIcon: {
@@ -19,6 +21,12 @@ export default {
       // These values come from your .env file or from environment variables set in Expo dashboard or eas.json
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    }
+      eas: {
+        projectId: "a758d76d-ba56-49b4-b7d9-786b02ad94f4"
+      }
+    },
+    plugins: [
+      "expo-router"
+    ]
   }
 };
