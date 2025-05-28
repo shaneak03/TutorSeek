591eb724557971fc6c0476ad7bf582648f8a639c
shaneak03 <shaneak03@gmail.com>
2025-05-28 08:26:09 +0800

    Add models, resolves #23

---
+++ utils/models.ts
@@ -0,0 +1,49 @@
+export interface TutorProfile {
+        id: string,
+        bio: string,
+        hourly_rate: number
+        is_published: boolean
+}
+
+export interface StudentProfile {
+        id: string,
+        name: string,
+        email: string,
+        enrolledCourses: string[],
+        completedCourses: string[]
+}
+
+export interface UserProfile {
+        id: string,
+        first_name: string,
+        last_name: string,
+        location: string,
+        role: "tutor" | "student"
+        // profileDetails: TutorProfile | StudentProfile
+}
+
+export interface TutorSubject {
+        id: string,
+        tutor_id: string,
+        subject_id: number,
+        level_id: number
+}
+
+export interface Subject {
+        id: number
+        name: string
+}
+
+export interface Level {
+        id: number
+        name: string
+}
+
+export interface Review {
+        id: number
+        created_at: Date
+        tutor_id: string,
+        student_id: string,
+        rating: number,
+        description: string
+}
